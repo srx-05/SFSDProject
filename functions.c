15f3b90ee47f1f14843090e33c8d2cd815b2f1ac
raw-djb <djebir.raounak@gmail.com>
2024-12-26 08:28:03 -0800

    Update functions.c

--- functions.c
+++ functions.c
@@ -11,40 +11,42 @@ void display_student(student *s) {
     printf("Group: %s\n", s->classe);
     printf("Grades:\n");
     for (int i = 0; i < 4; i++) {
-        printf("The subject %4s:\n \t %.2f (Coefficient: %d)\n",s->subjects[i].subj , s->subjects[i].note, s->subjects[i].coeff);
+        printf("The subject %4s:\n \t %.2f (Coefficient: %d)\n", s->subjects[i].subj , s->subjects[i].note, s->subjects[i].coeff);
     }
     printf("Average: %.2f\n", s->avg);
 }
-bool search_student(const char *FILENAME, int *i ) {
+
+bool search_student(const char *FILENAME, int *position) {
     int matricule;
-    *i=0;
+    *position = 0;  // Initialize position
+
     printf("\nEnter Matricule to search: ");
     scanf("%d", &matricule);
 
     FILE *file = fopen(FILENAME, "r");
     if (file == NULL) {
         printf("Error opening file.\n");
-        return 0;
+        return false;
     }
 
     student s;
     bool found = false;
     while (fscanf(file,
-            "%d %49s %49s %d %19s "
-            "%4s %d %f "
-            "%4s %d %f "
-            "%4s %d %f "
-            "%4s %d %f "
+            "%d %49s %49s %d %2s "
+            "%9s %f %d "
+            "%9s %f %d "
+            "%9s %f %d "
+            "%9s %f %d "
             "%f %d",
             &s.id, s.familyname, s.firstname, &s.yearofbirth, s.classe,
-            s.subjects[0].subj,  &s.subjects[0].coeff, &s.subjects[0].note,
-            s.subjects[1].subj,  &s.subjects[1].coeff, &s.subjects[1].note,
-            s.subjects[2].subj,  &s.subjects[2].coeff, &s.subjects[2].note,
-            s.subjects[3].subj,  &s.subjects[3].coeff, &s.subjects[3].note,
-            &s.avg, (int *)&s.exist)==19 ) {
-                      (*i)++;
-        if (s.id == matricule && !s.exist) {
+            s.subjects[0].subj,  &s.subjects[0].note, &s.subjects[0].coeff,
+            s.subjects[1].subj,  &s.subjects[1].note, &s.subjects[1].coeff,
+            s.subjects[2].subj,  &s.subjects[2].note, &s.subjects[2].coeff,
+            s.subjects[3].subj,  &s.subjects[3].note, &s.subjects[3].coeff,
+            &s.avg, (int *)&s.exist) == 19) {
 
+        (*position)++;
+        if (s.id == matricule && s.exist == 0) {  // Make sure to use `s.exist == false`
             found = true;
             break;
         }
@@ -53,14 +55,25 @@ bool search_student(const char *FILENAME, int *i ) {
 
     if (!found) {
         printf("Student not found.\n");
-
-    }
-    else {
+    } else {
         display_student(&s);
     }
+
     return found;
 }
 
+int main() {
+    int position;
+    bool student_found = search_student("students2.txt", &position);
+
+    if (student_found) {
+        printf("The position: %d\n", position);
+    } else {
+        printf("Student not found.\n");
+    }
+
+    return 0;
+}
 
 
 
