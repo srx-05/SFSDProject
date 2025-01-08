bd18e8356de73e4b569385e6f7eb22c0b44bb290
Ime32 <ai.diffallah@ensta.edu.dz>
2025-01-08 22:16:50 +0100

    Update main.c

--- main.c
+++ main.c
@@ -264,20 +264,38 @@ void addStudent(student **head, int last_id) {
     scanf("%s", newStudent.Class);
 
     // Input student marks with validation
-
     do {
-        printf("Enter the mark of this student in SFSD:\n");
+        printf("Enter the mark of this student in SFSD (0-20): \n");
         scanf("%f", &newStudent.subjects[0].note);
-        printf("Enter the mark of this student in POO:\n");
+
+         if (newStudent.subjects[0].note < 0 || newStudent.subjects[0].note > 20)
+              printf("Invalid marks. Please enter marks between 0 and 20.\n");
+
+    }while(newStudent.subjects[0].note < 0 || newStudent.subjects[0].note > 20);
+
+   do{
+       printf("Enter the mark of this student in POO (0-20): \n");
         scanf("%f", &newStudent.subjects[1].note);
-        printf("Enter the mark of this student in ANALYSIS:\n");
+       if(newStudent.subjects[1].note < 0 || newStudent.subjects[1].note > 20)
+           printf("Invalid marks. Please enter marks between 0 and 20.\n");
+
+   }while(newStudent.subjects[1].note < 0 || newStudent.subjects[1].note > 20);
+
+do{
+    printf("Enter the mark of this student in ANALYSIS (0-20): \n");
         scanf("%f", &newStudent.subjects[2].note);
-        printf("Enter the mark of this student in Linear Algebra:\n");
+    if(newStudent.subjects[2].note < 0 || newStudent.subjects[2].note > 20)
+        printf("Invalid marks. Please enter marks between 0 and 20.\n");
+
+}while(newStudent.subjects[2].note < 0 || newStudent.subjects[2].note > 20);
+
+do{
+    printf("Enter the mark of this student in Algebra (0-20): \n");
         scanf("%f", &newStudent.subjects[3].note);
-    } while (((newStudent.subjects[0].note < 0) || (newStudent.subjects[0].note > 20)) ||
-             ((newStudent.subjects[1].note < 0) || (newStudent.subjects[1].note > 20)) ||
-             ((newStudent.subjects[2].note < 0) || (newStudent.subjects[2].note > 20)) ||
-             ((newStudent.subjects[3].note < 0) || (newStudent.subjects[3].note > 20)));
+    if (newStudent.subjects[3].note < 0 || newStudent.subjects[3].note > 20)
+        printf("Invalid marks. Please enter marks between 0 and 20.\n");
+
+}while(newStudent.subjects[3].note < 0 || newStudent.subjects[3].note > 20);
 
     // Assign subject names and coefficients
     strcpy(newStudent.subjects[0].subj, "SFSD");
